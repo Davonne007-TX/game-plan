338806138ff3cdaeed95116a24727cf2df3bd635
Vonney <davonnejv@gmail.com>
2024-05-20 17:35:11 -0500

    adding notes from guidance counselor

---
+++ src/data/desseyn/chris.js
@@ -0,0 +1,25 @@
+/* Live: Taylor Desseyn and Chris Richardson: cloud: cyber security 
+Coh.org 
+
+- Urgently curious
+- Authentic self
+- Read the room
+- Be specific for each person
+- Do things out of your comfort zone
+- Stay up to speed with technologies
+- Seek human experiences
+- How to envolve my career
+- The Way of Kings: Book 
+- Men in full show
+- Double click on that feeling, ive had a lot of those feelings
+- take that risk that that shot take that chance 
+
+
+
+
+
+
+
+
+
+*/
