d3c80ae0e3b9ee91bca6802c24f5fd568059b32d
Vonney <davonnejv@gmail.com>
2024-06-10 15:40:45 -0500

    add more steps to do

--- src/components/StepByStep.jsx
+++ src/components/StepByStep.jsx
@@ -2,11 +2,13 @@ export default function StepByStep() {
   return (
     <>
       <h2 className="text-2xl text-pink-400 font-sixty">Step By Step:</h2>
-      <ul className="text-xl">
+      <ul className="text-xl mb-10">
         <li>JavaScript Basics</li>
         <li>JavaScript Questions</li>
         <li>React Scrimba</li>
         <li>AWS</li>
+        <li>What is Next.js?</li>
+        <li>TypeScript Basics</li>
       </ul>
     </>
   );
