05ac66adbd823288904f209ed39415cb82ae3485
Vonney <davonnejv@gmail.com>
2024-06-10 15:36:07 -0500

    feature: gamePlan 7 responsive for mobile devices

--- src/components/GamePlan7.jsx
+++ src/components/GamePlan7.jsx
@@ -4,13 +4,16 @@ export default function GamePlan7() {
       <main className="flex flex-col justify-center items-center text-white">
         <h1 className="text-orange-200 text-4xl font-serif">Game Plan #7</h1>
         <section className="flex flex-col justify-center items-center md:max-w-3xl lg:max-w-5xl mt-10 font-bold">
-          <h2 className="text-3xl mb-10"> Javascript Javascript Javascript</h2>
+          <h2 className="text-2xl lg:text-3xl mb-10">
+            {" "}
+            Javascript Javascript Javascript
+          </h2>
           <iframe
             src="https://giphy.com/embed/ZpdxXk2KXQn40VWqke"
             width="480"
             height="270"
             frameBorder="0"
-            className="giphy-embed w-90"
+            className="giphy-embed w-90 max-w-xs md:max-w-lg lg:max-w-xl"
             allowFullScreen
           ></iframe>
           <p>
@@ -19,21 +22,25 @@ export default function GamePlan7() {
             </a>
           </p>
 
-          <p className="text-2xl font-thin mt-10 max-w-xs md:max-w-3xl lg:max-w-4xl">
-            This is the focus right now. I know JavaScript is a powerful
-            language. I want to stick to what I am good at. And I excel in
-            Frontend Development. I want to be really good at what I do. So the
-            goal is Javascript, React.js, of course HTML, CSS. As well as
-            accessibility and diving into UI/UX. Baby steps. But the main goal
-            after all this is to build a full stack app. I am familiar with the
-            PERN stack so that is what I am going to do. I would need to think
-            of a quality project I would want to. That would ultimately be on my
-            resume. I want to be able to say Thank You Javascript. With all this
-            in mind, I need to think about what stack is in my area. I know Java
-            is popular as well TypeScript. It would be a good idea to implement
-            TypeScript in my project. If this is what it takes to land a job, I
-            am all in.
-          </p>
+          <section className="flex flex-col gap-8 max-w-xs md:max-w-3xl lg:max-w-4xl text-xl lg:text-2xl font-thin mt-10 ">
+            <p>
+              This is the focus right now. I know JavaScript is a powerful
+              language. I want to stick to what I am good at. And I excel in
+              Frontend Development. I want to be really good at what I do. So
+              the goal is Javascript, React.js, of course HTML, CSS. As well as
+              accessibility and diving into UI/UX. Baby steps. But the main goal
+              after all this is to build a full stack app. I am familiar with
+              the PERN stack so that is what I am going to do.
+            </p>
+            <p>
+              I would need to think of a quality project I would want to. That
+              would ultimately be on my resume. I want to be able to say Thank
+              You Javascript. With all this in mind, I need to think about what
+              stack is in my area. I know Java is popular as well TypeScript. It
+              would be a good idea to implement TypeScript in my project. If
+              this is what it takes to land a job, I am all in.
+            </p>
+          </section>
         </section>
       </main>
     </>
