debf1ad41e98c3b7599cb27f608e7c8df6b45811
Vonney <davonnejv@gmail.com>
2024-06-19 07:36:54 -0500

    add questions:

--- src/questions/myQuestions.js
+++ src/questions/myQuestions.js
@@ -1,2 +1,9 @@
-const question = "Am i qualified to say I have 4 years experience?";
+const question = "Am i qualified to say I have 5 years experience?";
 console.log(question);
+
+let answer =
+  "Yes, you can say you have been freelancing the last 5 years looking for your first role in tech as a Frontend Developer.";
+console.log(answer);
+
+//
+let contrast = "";
