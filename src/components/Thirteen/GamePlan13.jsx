5c23b805dc0cdc7d1f1ee0fb755b37d6e9c765f7
Davonne <davonnejv@gmail.com>
2024-09-18 21:41:07 -0500

    feature mobile responsiveness

--- src/components/Thirteen/GamePlan13.jsx
+++ src/components/Thirteen/GamePlan13.jsx
@@ -1,25 +1,29 @@
+import InProgress from "./InProgress";
+
 export default function GamePlan13() {
   const laptop = "../images/laptop.jpg";
 
   return (
     <>
-      <section className="flex-time text-white text-lg md:text-2xl">
+      <main className="flex-time ml-10 md:ml-0  text-white text-lg md:text-2xl font-lobster">
         <section>
           <h2 className="font-bold text-2xl md:text-3xl lg:text-4x mb-10">
             Game Plan #13
           </h2>
-          <p className=" font-lobster">
+          <p>
             Working on the same things from Game Plan 12 in regards to the first
             section.
             <br />
             With the emphasis on Typescript, JavaScript and React.
           </p>
+
+          <InProgress />
           <img
             src={laptop}
             className="max-w-sm md:max-w-xl lg:max-w-2xl mt-10"
           />
         </section>
-      </section>
+      </main>
     </>
   );
 }
--- src/components/Thirteen/InProgress.jsx
+++ src/components/Thirteen/InProgress.jsx
@@ -1,12 +1,16 @@
-
-
 export default function InProgress() {
-    const rightNow = [
-        { inProgress: "Javascript Problem Solving", id: 0 },
-        { inProgress: "Building Frontend Projects", id: 1 },
-        { inProgress: "Learning TypeScript", id: 2 },
-      ];
+  const rightNow = [
+    { inProgress: "Javascript Problem Solving", id: 0 },
+    { inProgress: "Building Frontend Projects", id: 1 },
+    { inProgress: "Learning TypeScript", id: 2 },
+    { inProgress: "React Documentation", id: 3 },
+  ];
   return (
-    
-  )
+    <section className="mt-20">
+      <p className="font-bold">In Progress</p>
+      {rightNow.map((now) => (
+        <li key={now.id}>{now.inProgress}</li>
+      ))}
+    </section>
+  );
 }
