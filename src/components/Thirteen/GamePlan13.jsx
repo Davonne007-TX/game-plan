3f761cd5376f011c0bd57101bde2b9ad372205b5
Davonne <davonnejv@gmail.com>
2024-09-23 12:31:08 -0500

    update Game Plan 13 mobile responsiveness

--- src/components/Thirteen/GamePlan13.jsx
+++ src/components/Thirteen/GamePlan13.jsx
@@ -5,17 +5,16 @@ export default function GamePlan13() {
 
   return (
     <>
-      <main className="flex-time ml-10 md:ml-0 text-white text-lg md:text-2xl">
-        <section>
+      <main className="max-w-sm md:max-w-2xl ml-auto mr-auto text-white text-lg md:text-2xl">
+        <section className="flex flex-col gap-2">
           <h2 className="font-bold text-2xl md:text-3xl lg:text-4x mb-10">
             Game Plan #13
           </h2>
           <p>
             Working on the same things from Game Plan 12 in regards to the first
             section.
-            <br />
-            With the emphasis on Typescript, JavaScript and React.
           </p>
+          <p>With the emphasis on Typescript, JavaScript and React.</p>
 
           <InProgress />
           <img
