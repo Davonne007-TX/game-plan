dd17b15c7d813689ab9d39de5c33086804492f69
Davonne <davonnejv@gmail.com>
2024-10-15 14:43:33 -0500

    add useEffect hook that runs whenever countDownStarted and/or the eventDate changes

---
+++ src/components/more-examples/CountDown/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="flex-time">
+      <h1 className="font-bold font-lobster text-4xl">Countdown Timer</h1>
+      <p className="mt-2 text-2xl">Brought to you by: freeCodeCamp</p>
+    </header>
+  );
+}
--- src/components/more-examples/CountDown/Timer.jsx
+++ src/components/more-examples/CountDown/Timer.jsx
@@ -1,14 +1,34 @@
-import { useState } from "react";
+import Header from "./Header";
+import { useState, useEffect } from "react";
 
 export default function Timer() {
-  const [eventName, setEventName] = useState("");
-  const [eventDate, setEventDate] = useState("");
-  const [countDownStarted, setCountDownStarted] = useState("");
-  const [timeRemaining, setTimeRemaining] = useState(0);
+  const [eventName, setEventName] = useState(""); //stores the name of the event
+  const [eventDate, setEventDate] = useState(""); //stores the date of the event for the countdown timer
+  const [countDownStarted, setCountDownStarted] = useState(""); //tracks whether the countdown timer has started
+  const [timeRemaining, setTimeRemaining] = useState(0); //shows the time remaining in milliseconds
+
+  useEffect(() => {
+    if (countDownStarted && eventDate) {
+      const countDownInterval = setInterval(() => {
+        const currentTime = new Date().getTime();
+        const eventTime = new Date(eventTime).getTime();
+        let remainingTime = eventTime - currentTime;
+
+        if (remainingTime <= 0) {
+          remainingTime = 0;
+          clearInterval(countDownInterval);
+          alert("Countdown Complete");
+        }
+        setTimeRemaining(remainingTime);
+      }, 1000);
+
+      return () => clearInterval(countDownInterval);
+    }
+  }, [countDownStarted, eventDate, timeRemaining]);
 
   return (
-    <section className="flex-time">
-      <h1 className="font-bold font-lobster text-4xl">Countdown Timer</h1>
+    <section>
+      <Header />
     </section>
   );
 }
