a925296c2ad9adaec0e7520157083de5615011f1
Davonne <davonnejv@gmail.com>
2024-10-15 15:22:04 -0500

    project needs some flavor. feature: tailwind styles

--- src/components/more-examples/CountDown/Header.jsx
+++ src/components/more-examples/CountDown/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="flex-time">
       <h1 className="font-bold font-lobster text-4xl">Countdown Timer</h1>
-      <p className="mt-2 text-2xl">Brought to you by: freeCodeCamp</p>
+      <p className="mt-2 text-xl">Brought to you by: freeCodeCamp</p>
     </header>
   );
 }
--- src/components/more-examples/CountDown/Timer.jsx
+++ src/components/more-examples/CountDown/Timer.jsx
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import { useState, useEffect } from "react";
+import Button from "../../Button";
 
 export default function Timer() {
   const [eventName, setEventName] = useState(""); //stores the name of the event
@@ -34,11 +35,13 @@ export default function Timer() {
   }, [countDownStarted, eventName]);
 
   return (
-    <main className="max-w-7xl bg-gradient-to-r from-pink-400 to-white p-8 rounded-xl ml-auto mr-auto">
+    <main className="max-w-5xl bg-gradient-to-r font-serif font-thin text-md md:text-lg lg:text-xl from-pink-200 to-pink-500 p-8 rounded-xl ml-auto mr-auto">
       <Header />
 
-      <h2>{countDownStarted ? eventName : "Countdown Timer"}</h2>
-      <form>
+      <h2 className="text-2xl mb-2 mt-4">
+        {countDownStarted ? eventName : "Countdown Begins"}
+      </h2>
+      <form className="flex flex-col gap-4">
         <label> Event Name:</label>
         <input
           name="title"
@@ -46,6 +49,7 @@ export default function Timer() {
           placeholder="Enter Event Name"
           value={eventName}
           onChange={(e) => setEventName(e.target.value)}
+          className="p-2 rounded-xl"
         />
         <label>Event Date:</label>
         <input
@@ -54,9 +58,13 @@ export default function Timer() {
           value={eventDate}
           onChange={(e) => setEventDate(e.target.value)}
           onClick={(e) => (e.target.type = "date")}
+          className="p-2 rounded-xl"
         />
 
-        <button>Start Countdown</button>
+        <Button
+          buttonText="Start Countdown"
+          className="bg-white w-40 h-10 text-sm md:text-md lg:text-lg hover:text-pink-500"
+        />
       </form>
     </main>
   );
