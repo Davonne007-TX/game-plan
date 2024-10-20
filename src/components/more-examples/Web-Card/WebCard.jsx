cd5c9f87800882d0cbb1d8b9f2d36617e40e8e2a
Davonne <davonnejv@gmail.com>
2024-10-19 20:34:53 -0500

    add image with styling and mobile responsiveness

--- src/components/more-examples/Web-Card/WebCard.jsx
+++ src/components/more-examples/Web-Card/WebCard.jsx
@@ -3,17 +3,21 @@ import Helpful from "./Helpful";
 import HireMe from "./HireMe";
 
 export default function WebCard() {
+  const me = "./images/me.jpeg";
   return (
     <main className="flex flex-col justify-center items-center mt-20 font-serif">
       <h2 className="text-white text-3xl font-bold">UI Web Card </h2>
       <p className="text-white text-sm md:text-xl">
         From post by @janm_ux Horizontal UI Web Card
       </p>
-      <section className="bg-white w-full max-w-sm md:max-w-2xl p-8 rounded-lg shadow-md mt-20">
-        <section className="flex flex-col gap-8">
-          <Name name="Davonne V." />
-          <Helpful />
-          <HireMe />
+      <section className="bg-white w-full max-w-sm md:max-w-5xl p-8 rounded-lg shadow-md mt-10 md:mt-20">
+        <section className="flex flex-col md:flex-row gap-8">
+          <img src={me} className="max-w-sm" />
+          <section className="flex flex-col gap-8">
+            <Name name="Davonne V." />
+            <Helpful />
+            <HireMe />
+          </section>
         </section>
       </section>
     </main>
