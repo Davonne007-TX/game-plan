a727817bee76bb21e700fc00ec137bb57311c4b6
Davonne <davonnejv@gmail.com>
2024-11-12 19:23:06 -0600

    update Web Card, margin bottom

--- src/components/more-examples/Web-Card/WebCard.jsx
+++ src/components/more-examples/Web-Card/WebCard.jsx
@@ -3,16 +3,16 @@ import Helpful from "./Helpful";
 import HireMe from "./HireMe";
 
 export default function WebCard() {
-  const me = "./images/me.jpeg";
+  const thisIsMe = "./images/me.jpeg";
   return (
     <main className="flex flex-col justify-center items-center mt-20 font-serif">
       <h2 className="text-white text-3xl font-bold">UI Web Card </h2>
       <p className="text-white text-sm md:text-xl">
         From post by @janm_ux Horizontal UI Web Card
       </p>
-      <section className="bg-white w-full max-w-sm md:max-w-5xl p-8 rounded-lg shadow-md mt-10 md:mt-20">
+      <section className="bg-white w-full mb-10 max-w-sm md:max-w-5xl p-8 rounded-lg shadow-md mt-10 md:mt-20">
         <section className="flex flex-col md:flex-row gap-8">
-          <img src={me} className="max-w-sm" />
+          <img src={thisIsMe} className="max-w-sm" />
           <section className="flex flex-col gap-8">
             <Name name="Davonne V." />
             <Helpful />
