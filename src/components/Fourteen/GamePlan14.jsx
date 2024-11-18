9d4b3914d46ac03aea119c2e0b9e2e12b91cb2aa
Davonne <davonnejv@gmail.com>
2024-11-18 11:14:40 -0600

    add more key takeaways

--- src/components/Fourteen/GamePlan14.jsx
+++ src/components/Fourteen/GamePlan14.jsx
@@ -3,7 +3,7 @@ import Podcast1 from "./podcast1";
 function GamePlan14() {
   return (
     <>
-      <h2 className="text-green-600 text-center font-bold text-2xl md:text-3xl lg:text-4xl mb-10  ">
+      <h2 className="text-green-500 text-center font-bold text-2xl md:text-3xl lg:text-4xl mb-10  ">
         Game Plan 14 <br />
         <span className="text-xl font-mono">
           Listening To The Programming Podcast <br />
--- src/components/Fourteen/Podcast1.jsx
+++ src/components/Fourteen/Podcast1.jsx
@@ -1,12 +1,12 @@
 export default function Podcast1() {
   const keyPoints = [
-    { point: "Make tutorials your own", id: 1 },
+    { point: "Make tutorials your own!!!", id: 1 },
     { point: "What is your local market?", id: 2 },
     { point: "Soft skills can also get you the first job", id: 3 },
-    { point: "Play the Game, The game exist", id: 4 },
+    { point: "Play the game, The game exist", id: 4 },
     { point: "Stand out in the market", id: 5 },
     {
-      point: "Build Your Network,",
+      point: "Build Your Network",
       id: 6,
     },
     {
@@ -16,24 +16,37 @@ export default function Podcast1() {
     { point: "Network, network, network", id: 8 },
     { point: "Invest in Your Growth", id: 9 },
     { point: "Learn How To Play The Game", id: 10 },
-    { point: "Setup LinkedIn, Know your story" },
-    { point: "" },
-    { point: "" },
-    { point: "" },
-    { point: "" },
-    { point: "" },
-    { point: "" },
+    { point: "Setup LinkedIn, Know your story", id: 11 },
+    {
+      point: "Solve Problems, Deliver Solutions, Talk About the Solution",
+      id: 12,
+    },
+    { point: "Break through the barriers", id: 13 },
+    { point: "Build projects worth talking about", id: 14 },
+    { point: "Make projects your own", id: 15 },
+    { point: "Projects from within, Solve your own problems", id: 16 },
+    { point: "Know your audience for your projects", id: 17 },
+    { point: "Freelance Projects, Passion Projects", id: 18 },
+    {
+      point: "Be more detailed on Experience Section, What did you do?",
+      id: 19,
+    },
+    {
+      point:
+        "Make first and second point on Resume a home run so recruiters keep reading, Peak their curiosity ",
+      id: 20,
+    },
   ];
   return (
     <>
-      <section className="text-white flex flex-col justify-center items-center m-0">
+      <section className="text-white flex flex-col justify-center items-center m-0 mb-20">
         <h2 className="font-bold text-3xl font-mono">
           The Programming Podcast Key Takeaways
         </h2>
 
-        <ul className="mt-8 text-2xl">
+        <ul className="mt-8 text-2xl ml-10">
           {keyPoints.map((points) => (
-            <li key={points.id}>{points.point}</li>
+            <li key={points.id}>🔥 {points.point}</li>
           ))}
         </ul>
       </section>
