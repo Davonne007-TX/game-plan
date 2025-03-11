f910cc1059a51b713b16020eb21cd94b0ca3bf3f
Davonne Vigil <davonnejv@gmail.com>
2025-03-11 12:27:04 -0500

    feature: useRef hook example

--- src/components/Hooks/useRef/HookUseRef.jsx
+++ src/components/Hooks/useRef/HookUseRef.jsx
@@ -1,7 +1,21 @@
+import { useRef } from "react";
+
 export default function HookUseRef() {
+  let ref = useRef(0);
+
+  function handleClick() {
+    ref.current = ref.current + 1;
+    alert(`You clicked ` + ref.current + " times..!");
+  }
   return (
     <section className="flex-time">
       <h2 className="font-pizza text-2xl text-white">Hook useRef</h2>;
+      <button
+        onClick={handleClick}
+        className="bg-green-500 rounded-full mt-20 p-2 text-3xl font-pizza hover:scale-110"
+      >
+        Click Me and see what happens..!
+      </button>
     </section>
   );
 }
