7be4653342eedb553062e395dbc6718d7d3aae8e
Davonne Vigil <davonnejv@gmail.com>
2025-05-31 23:32:08 -0500

    add functions to handle ref and state updates from onClick styled buttons

--- src/components/Hooks/useRef/LearnRefs.jsx
+++ src/components/Hooks/useRef/LearnRefs.jsx
@@ -1,8 +1,17 @@
 import { useRef, useState } from "react";
+import Button from "../../Button";
 
 export default function LearnRefs() {
   const ref = useRef("Hola amiga!");
   const [text, setText] = useState("Hola amiga");
+
+  const handleRefUpdate = () => {
+    ref.current = "Hola, como estas";
+  };
+
+  const handleStateUpdate = () => {
+    setText("Hola como estas");
+  };
 
   return (
     <section className="flex flex-col justify-center items-center">
@@ -16,12 +25,24 @@ export default function LearnRefs() {
         </a>
       </h1>
 
-      <div className="bg-pink-500 rounded text-2xl mt-8 font-anton max-w-5xl px-4">
+      <div className="bg-pink-500 rounded text-2xl mt-8 font-anton max-w-5xl px-4 py-4">
         <p>Ref: {ref.current}</p>
         <p>State: {text}</p>
+        <div className="flex gap-4 mt-8 px-4">
+          <Button
+            buttonText="Update ref"
+            onClick={handleRefUpdate}
+            className="p-2 bg-white hover:scale-105 transition-all duration-300 transform ease-out hover:bg-gray-200 text-pink-500"
+          />
+          <Button
+            buttonText="Update State"
+            onClick={handleStateUpdate}
+            className="p-2 bg-white hover:scale-105 transition-all duration-300 transform ease-out hover:bg-gray-200 text-pink-500"
+          />{" "}
+        </div>
       </div>
     </section>
   );
 }
 
-//useState vs useRef
+//useState vs useRef updates
