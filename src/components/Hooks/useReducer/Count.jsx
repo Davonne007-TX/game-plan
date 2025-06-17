0891b5bf7b5119f1f3b827389075498de379c8ce
Davonne Vigil <davonnejv@gmail.com>
2025-06-16 23:16:24 -0500

    add useReducer hook to set the count

--- src/components/Hooks/useReducer/Count.jsx
+++ src/components/Hooks/useReducer/Count.jsx
@@ -1,4 +1,19 @@
+import { useReducer } from "react";
+import Button from "../../Button";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+}
+
 export default function Count() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
   return (
     <section className="flex flex-col justify-center items-center">
       <h1 className="text-white font-extrabold text-3xl text-center">
@@ -7,6 +22,22 @@ export default function Count() {
       <p className="text-purple-600 font-bold mt-2 text-2xl">
         with Switch Statement
       </p>
+
+      <div className="text-white mt-20 text-4xl font-thin">
+        <p>Count: {state.count}</p>
+        <div className="text-center text-5xl">
+          <Button
+            buttonText="-"
+            onClick={() => dispatch({ type: "decrement" })}
+            className="text-white hover:scale-105"
+          />
+          <Button
+            buttonText="+"
+            className="hover:scale-105"
+            onClick={() => dispatch({ type: "increment" })}
+          />
+        </div>
+      </div>
     </section>
   );
 }
