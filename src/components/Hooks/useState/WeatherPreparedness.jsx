0218d4eb6b7e0caa3adcc5f5efc484d66e352bcb
Davonne Vigil <davonnejv@gmail.com>
2025-06-26 21:47:11 -0500

    add UI, set up state, display description for torando

--- src/components/Hooks/useState/WeatherPreparedness.jsx
+++ src/components/Hooks/useState/WeatherPreparedness.jsx
@@ -1,9 +1,43 @@
+import { useState } from "react";
+import Button from "../../Button";
+import { whatToDoWeatherEvent } from "./whatToDoWeatherEvent ";
+
 export default function WeatherPreparedness() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+  const next = index < whatToDoWeatherEvent.length - 1;
+
+  const handleClick = () => {
+    if (next) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
+  };
+
+  const handleMoreWeatherPrep = () => {
+    setShowMore(!showMore);
+  };
+
+  let storm = whatToDoWeatherEvent[index];
   return (
-    <section className="flex flex-col justify-center items-center">
-      <h className="text-2xl md:text-4xl font-anton text-blue-600">
+    <section className="flex flex-col justify-center  text-white items-center">
+      <h1 className="text-2xl md:text-4xl font-anton text-blue-600">
         Weather Preparedness Guide
-      </h>
+      </h1>
+
+      <div className="max-w-xs md:max-w-4xl p-2 mt-20 flex flex-col justify-center items-center gap-4">
+        <Button
+          buttonText="Next"
+          className="bg-white w-40 font-anton tracking-widest text-black p-2 rounded font-bold"
+          onClick={handleClick}
+        />
+        <h2 className="font-sixty text-2xl">{storm.name}</h2>
+        <p className="font-serif text-center text-lg md:text-xl">
+          {storm.description}
+        </p>
+        <img src={storm.img} alt={storm.alt} />
+      </div>
     </section>
   );
 }
