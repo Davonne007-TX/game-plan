a409c8e77a4f903fb7b1e86ad380ebb35b5be607
Davonne Vigil <davonnejv@gmail.com>
2025-07-26 17:45:16 -0500

    lets do aguachile instead

--- src/components/more-examples/seafood-reciepe/Ceviche.jsx
+++ src/components/more-examples/seafood-reciepe/Ceviche.jsx
@@ -13,16 +13,15 @@ export default function Ceviche() {
     <section className="flex flex-col justify-center items-center">
       <div className="max-w-4xl my-10 bg-white rounded-3xl p-4 flex flex-col justify-center items-center">
         <h2 className="text-2xl md:text-4xl font-bold font-lobster text-blue-500">
-          Ceviche Ingredigent List
+          Aguachile Ingredigent Check List
         </h2>
-        <p className="text-xl">✅ means got my item!</p>
+        <p className="text-xl">✅ means in stock</p>
 
         <ul className="flex flex-col mr-auto gap-10 text-2xl mt-10 font-thin">
           <Ingredigent inStock={true} name="Shrimp 🍤" />
           <Ingredigent niStock={false} name="Red Onion 🟥 🧅" />
           <Ingredigent inStock={false} name="Cucumbers 🥒 " />
           <Ingredigent inStock={true} name="Limes 🍋‍🟩" />
-          <Ingredigent inStock={false} name="Tomatoes 🍅" />
           <Ingredigent inStock={false} name="Serranno Peppers 🔥" />
         </ul>
 
