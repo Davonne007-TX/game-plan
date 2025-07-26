eedf1a0edab416004dfbe8a1b3e7d61396e4c674
Davonne Vigil <davonnejv@gmail.com>
2025-07-26 00:44:04 -0500

    update li in Ingredient function to return a ternary operator

--- src/components/more-examples/seafood-reciepe/Ceviche.jsx
+++ src/components/more-examples/seafood-reciepe/Ceviche.jsx
@@ -1,22 +1,28 @@
+import Image from "../../Image";
+
 function Ingredigent({ name, inStock }) {
-  return <li>{name}</li>;
+  return <li>{inStock ? name + " ✅ " : name}</li>;
 }
 
 export default function Ceviche() {
   return (
     <section className="flex flex-col justify-center items-center">
-      <div className="w-96 bg-white rounded-3xl p-4 flex flex-col justify-center items-center">
+      <div className="max-w-4xl bg-white rounded-3xl p-4 flex flex-col justify-center items-center">
         <h2 className="text-2xl md:text-3xl font-bold font-lobster text-blue-500">
           Ceviche Ingredigent List
         </h2>
 
         <ul className="flex flex-col mr-auto gap-10 text-2xl mt-10 font-thin">
-          <Ingredigent inStock={true} name="Shrimp" />
-          <Ingredigent niStock={true} name="Red Onion" />
-          <Ingredigent inStock={false} name="Cucumbers" />
-          <Ingredigent inStock={true} name="Limes" />
-          <Ingredigent inStock={false} name="Tomatoes" />
+          <Ingredigent inStock={true} name="Shrimp 🍤" />
+          <Ingredigent niStock={false} name="Red Onion 🟥 🧅" />
+          <Ingredigent inStock={false} name="Cucumbers 🥒 " />
+          <Ingredigent inStock={true} name="Limes 🍋‍🟩" />
+          <Ingredigent inStock={false} name="Tomatoes 🍅" />
         </ul>
+
+        <div className="mt-10 max-w-sm">
+          <Image src="./images/ceviche.png" />
+        </div>
       </div>
     </section>
   );
