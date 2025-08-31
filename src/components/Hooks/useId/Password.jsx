c7f6ce59c39bc5f05bfe82230e47ac6b630a367d
Davonne Vigil <davonnejv@gmail.com>
2025-08-30 21:53:34 -0500

    feature: back to hooks button:

--- src/components/Hooks/useId/Password.jsx
+++ src/components/Hooks/useId/Password.jsx
@@ -1,6 +1,7 @@
 import ChoosePassword from "./ChoosePassword";
+import ReuseableButton from "../../ReuseableButton";
 
-export default function Password() {
+export default function Password({ backToHooks }) {
   return (
     <section className="flex-time">
       <h1 className="font-capri text-purple-300 mt-4 text-4xl">
@@ -9,6 +10,10 @@ export default function Password() {
 
       <ChoosePassword />
       <ChoosePassword />
+
+      <section className=" mt-20 flex gap-8 justify-center items-center">
+        <ReuseableButton label="Back To Hooks" onClick={backToHooks} />
+      </section>
     </section>
   );
 }
