6eade01f04d067f8d8b6b737df260deb8fe7cb76
Davonne Vigil <davonnejv@gmail.com>
2025-09-28 20:37:56 -0500

    Opps..! Looking at the code, I was setting up a function Status and styling

--- src/components/Hooks/useDebugValue/OnlineStatus.jsx
+++ src/components/Hooks/useDebugValue/OnlineStatus.jsx
@@ -3,7 +3,7 @@ import { useOnlineStatus } from "./useOnlineStatus";
 function Status() {
   const isOnline = useOnlineStatus();
   return (
-    <h1 className="mt-20 text-3xl font-thin p-2">
+    <h1 className="mt-20 text-3xl font-thin p-2 tracking-widest bg-white">
       {isOnline ? "✅ Online" : "❌ Disconnected"}
     </h1>
   );
@@ -15,9 +15,7 @@ export default function OnlineStatus() {
       <div className="max-w-sm md:max-w-3xl p-8 mt-10">
         <h2 className="font-anton text-white text-2xl">useDebugValue</h2>
 
-        <div className="bg-white">
-          <Status />
-        </div>
+        <Status />
       </div>
     </section>
   );
